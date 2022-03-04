c1cd258ab834f987e25896680ded6ff8299bc64c
AlmasBanuA <94681887+AlmasBanuA@users.noreply.github.com>
2022-03-04 23:16:46 +0530

    Program to Finding the frequency of words and in large paragraph and removing of avoidable words from phrase

--- src/day15/HashTable/HashTable.java
+++ src/day15/HashTable/HashTable.java
@@ -1,20 +1,28 @@
 package day15.HashTable;
 
 /**
- * UC3- Remove avoidable word from the phrase �Paranoids are not paranoid
+ * UC1-Ability to find frequency of words in a sentence like “To be or not to be”
+ 
+ * UC2-Ability to find frequency of words in a large paragraph phrase 
+ * “Paranoids are not paranoid because they are paranoid but because 
+ * they keep putting themselves deliberately into paranoid avoidable situations”
+
+ * UC3- Remove avoidable word from the phrase “Paranoids are not paranoid
  * because they are paranoid but because they keep putting themselves
- * deliberately into paranoid avoidable situations�
+ * deliberately into paranoid avoidable situations”
  */
 
 /*
  * Procedure:
- * 1. Use hashcode to find index of the words in the para 
- * 2. Create LinkedList for each index and store the words and its frequency 
- * 3. Use LinkedList to do the Hash Table Operation 
- * 4. To do this create MyMapNode with Key Value Pair and create LinkedList of MyMapNode
- * 5. Use LinkedList to do the Hash Table Operation like here 
+ * 1.Use LinkedList to do the Hash Table Operation
+ * 2. Use hashcode to find index of the words in the para 
+ * 3. Create LinkedList for each index and store the words and its frequency 
+ * 4. Use LinkedList to do the Hash Table Operation 
+ * 5. To do this create MyMapNode with Key Value Pair and create LinkedList of MyMapNode
+ * 6. Use LinkedList to do the Hash Table Operation like here 
  *    the removal of word avoidable 
  */
+
 public class HashTable {
 
 	/**
