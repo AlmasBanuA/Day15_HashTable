1bd30af26d62cffb1c10a68bd66f6a40cd72ceb3
[AlmasBanuA2011] <[almasbanu3304@gmail.com]>
2022-03-04 22:41:03 +0530

    Finding Frequency of words in Paragraph

--- src/day15/HashTable/HashTable.java
+++ src/day15/HashTable/HashTable.java
@@ -1,9 +1,16 @@
 package day15.HashTable;
 
 /**
- * Ability to find frequency of words in a sentence like “To be or not to be”
- * Use LinkedList to do the Hash Table Operation To do this we create MyMapNode
- * with Key Value Pair and create LinkedList of MyMapNode
+ * UC2- Ability to find frequency of words in a large paragraph phrase 
+ * “Paranoids are not paranoid because they are paranoid but because 
+ * they keep putting themselves deliberately into paranoid avoidable situations”
+ */
+
+/* Procedure:
+ * 1. Use hashcode to find index of the words in the para
+ * 2. Create LinkedList for each index and store the words and its frequency
+ * 3. Use LinkedList to do the Hash Table Operation
+ * 4. To do this create MyMapNode with Key Value Pair and create LinkedList of MyMapNode
  */
 public class HashTable {
 
@@ -14,7 +21,9 @@ public class HashTable {
 	 */
 	public static void main(String[] args) {
 		HashTableImplementation<String, Integer> hashImpl = new HashTableImplementation<String, Integer>();
-		String message = "To be or not to be";
+		String message = "Paranoids are not\r\n" + "paranoid because they are paranoid but\r\n"
+				+ "because they keep putting themselves\r\n" + "deliberately into paranoid avoidable\r\n"
+				+ "situations";
 
 		/*
 		 * Split the words when a white space comes up and Convert all the words in
